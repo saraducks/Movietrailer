621e340e52b9d4943149c9261b145289f8824efd
saraducks <saranyadhakshina25@gmail.com>
2016-08-23 13:18:07 -0700

    Add files via upload

---
+++ movietrailer.py
@@ -0,0 +1,18 @@
+import webbrowser
+"""
+    The base class is Movie
+    The instance variables and instance methods are created
+    """
+
+class Movie:
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
+    
